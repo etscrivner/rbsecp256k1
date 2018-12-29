e55cdaaa49b284faa9ba175c3c825d230548abaf
Eric Scrivner <eric.t.scrivner@gmail.com>
2018-12-29 12:23:51 -0800

    Add RecoverableSignature Type
    Add the struct, data type, and basic class definition for the new
    `RecoverableSignature` type. Optionally include the recoverable signature code
    if the appropriate header is available.

--- ext/rbsecp256k1/extconf.rb
+++ ext/rbsecp256k1/extconf.rb
@@ -10,4 +10,7 @@ print("Looking for libsecp256k1")
 results = pkg_config('libsecp256k1')
 abort "missing libsecp256k1" unless results[1]
 
+# Check if we have the libsecp256k1 recoverable signature header.
+have_header('secp256k1_recovery.h')
+
 create_makefile('rbsecp256k1')
